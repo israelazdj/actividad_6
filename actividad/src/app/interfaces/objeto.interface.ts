5ce7b7c28fb5d66297161327577cd0de573127a5
Israel Arenas <israel.djaz@gmail.com>
2024-08-08 15:48:48 +0200

    se creraron los compoentes e interfaces

---
+++ actividad/src/app/interfaces/objeto.interface.ts
@@ -0,0 +1,18 @@
+export interface Objeto {
+    page:        number;
+    per_page:    number;
+    total:       number;
+    total_pages: number;
+    results:     User[];
+}
+
+export interface User {
+    _id:        string;
+    id:         number;
+    first_name: string;
+    last_name:  string;
+    username:   string;
+    email:      string;
+    image:      string;
+    password:   string;
+}
