8566361e647e429ebe9418c8d28eb542d52fe403
Israel Arenas <israel.djaz@gmail.com>
2024-08-09 17:58:22 +0200

    se crearon algunas rutas

--- actividad/src/app/app.routes.ts
+++ actividad/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { UserListComponent } from './pages/user-list/user-list.component';
+import { ControlPanelComponent } from './pages/control-panel/control-panel.component';
+import { UserviewComponent } from './pages/userview/userview.component';
+import { FormComponent } from './pages/form/form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:"", pathMatch:"full",redirectTo:"home"},
+    {path:"home",component:UserListComponent},
+    {path:"control-panel",component:ControlPanelComponent , children: [
+        {path:"",pathMatch: 'full',redirectTo: 'home'},
+        {path:"empleado/:id",component:UserviewComponent},
+        {path:"home", component:UserListComponent},
+        {path:"nuevo-empleado", component:FormComponent},
+        {path:"actualizar-empleado/:id", component:FormComponent}
+    ]},
+    
+    {path:"**",redirectTo:"home"}
+];
