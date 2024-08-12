75173b140c261e41ce1eaedced81e031c66a1b89
Israel Arenas <israel.djaz@gmail.com>
2024-08-12 13:35:51 +0200

    se agregaron estilos a la card-user y @media para quitar el borde a los 762px

--- actividad/src/app/pages/form/form.component.ts
+++ actividad/src/app/pages/form/form.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
