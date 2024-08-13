0d8d6c3271937e1e878e9432af9fcb4c2b5aa2ba
Israel Arenas <israel.djaz@gmail.com>
2024-08-13 11:48:18 +0200

    validaciones en el formulario

--- actividad/src/app/pages/form/form.component.ts
+++ actividad/src/app/pages/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -9,5 +9,40 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+  
+  usuarioform:FormGroup;
+
+  constructor(){
+    this.usuarioform= new FormGroup({
+      nombre: new FormControl(null,[
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern(/^[A-Za-z\s\xF1\xD1]+$/)
+      ]),
+      apellido: new FormControl(null,[
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern(/^[A-Za-z\s\xF1\xD1]+$/)
+      ]),
+      email: new FormControl(null,[
+        Validators.required,
+        Validators.pattern(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)
+      ]),
+      imagen: new FormControl(null,[
+        Validators.required,
+        Validators.pattern(/^(?:(http(s)?)?(sftp)?(ftp)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/)
+      ]),
+    })
+  
+  }
+
+  getDataForm(){}
+
+
+
+  checkControl(formControlName: string, Validator: string){
+    return this.usuarioform.get(formControlName)?.hasError(Validator) && this.usuarioform.get(formControlName)?.touched;
+
+  }
 
 }
