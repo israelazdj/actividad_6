0a75b244ba42c7b22343680de565a87011feecca
Israel Arenas <israel.djaz@gmail.com>
2024-08-16 14:08:56 +0200

    se agrego boton de volver desde el formulario

--- actividad/src/app/pages/userview/userview.component.ts
+++ actividad/src/app/pages/userview/userview.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { User } from '../../interfaces/objeto.interface';
 import { RouterLink } from '@angular/router';
 import { CardUserComponent } from '../card-user/card-user.component';
+import { UsuariosService } from '../../services/usuarios.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-userview',
@@ -12,4 +14,37 @@ import { CardUserComponent } from '../card-user/card-user.component';
 })
 export class UserviewComponent {
   @Input() myuser!: User;
+  userServices = inject(UsuariosService)
+  arruser: User[]=[]
+
+  async delete(id:string | undefined){
+    if(id){
+      let borrar= confirm('Estas seguro que quieres borrar el empleado: ' + id)
+      if(borrar)
+      {
+        try{
+        const response: User = await firstValueFrom(this.userServices.delete(id))
+        console.log(response)
+        if(response._id)
+        {
+          const response = await firstValueFrom(this.userServices.getAll())
+
+          this.arruser = response.results;
+          alert('Usuario borrado correctamente')
+
+        }
+      }
+      catch (error) {
+        console.log(error)
+        /* this.erroru = error
+        console.log(this.erroru) */
+      }
+      }
+
+    }
+
+
+
+  
+}
 }
